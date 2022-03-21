723b80cc3dbe568c13da8af8b8fba404b27903a3
priegels <88594441+priegels@users.noreply.github.com>
2022-03-21 15:25:19 +0100

    added functions for showing infos + adding movies to fav list

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GenreCardComponent } from '../genre-card/genre-card.component';
+import { DirectorCardComponent } from '../director-card/director-card.component';
+import { SynopsisCardComponent } from '../synopsis-card/synopsis-card.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  FavMovie: any[] = [];
+  Username = localStorage.getItem('user');
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    public router: Router
+   ) {}
 
   ngOnInit(): void {
     this.getMovies();
+    this.showFavMovie();
   }
 
+  // show all movies
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
@@ -21,4 +38,68 @@ export class MovieCardComponent {
       return this.movies;
     });
   }
+
+  // function to pull all favorited movies of a user.
+  showFavMovie(): void {
+    const user = localStorage.getItem('user');
+    this.fetchApiData.getUser(user).subscribe((resp: any) => {
+      this.FavMovie = resp.FavMovie;
+      console.log(this.FavMovie);
+    });
+  }
+
+  // function to let user add a movie to their favorite movies
+  addFavMovie(MovieID: any, Title: string): void {
+    this.fetchApiData.addFavMovie(MovieID).subscribe((resp: any) => {
+      console.log(resp);
+      this.snackBar.open(`${Title} has been added to your favorites.`, 'OK', {
+      duration: 3000,
+    });
+    this.showFavMovie();
+    });
+  }
+
+  // function to let user remove a movie from their favorite movies
+  deleteFavMovie(MovieID: any, Title: string): void {
+    this.fetchApiData.deleteFavMovie(MovieID).subscribe((resp: any) => {
+      console.log(resp);
+      this.snackBar.open(`${Title} is no longer favorited.`, 'OK', {
+        duration: 3000,
+      });
+      this.showFavMovie();
+    });
+  }
+
+  // function to display to user filled or empty heart icon depending on favorite status of a movie
+  setFavStatus(MovieID: any): any {
+    if (this.FavMovie.includes(MovieID)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // open Genre dialog
+  openGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreCardComponent, {
+      data: { name, description },
+      width: '500px'
+    });
+  }
+
+  // open Director dialog
+  openDirectorDialog(name: string, bio: string): void {
+    this.dialog.open(DirectorCardComponent, {
+      data: {name, bio},
+      width: '500px',
+    });
+  }
+
+  // open Synopsis dialog
+  openSynopsisDialog(title: string, description: string): void {
+    this.dialog.open(SynopsisCardComponent, {
+      data: { title, description}
+    });
+  }
+
 }
